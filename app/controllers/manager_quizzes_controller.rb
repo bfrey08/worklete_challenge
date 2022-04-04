6a0d1c6102ad52e543fed72f6fb9fc401f84635e
Billy Frey <bfrey16@gmail.com>
2022-04-04 14:47:30 -0600

    complete manager quiz index controller for issue # 5

--- app/controllers/manager_quizzes_controller.rb
+++ app/controllers/manager_quizzes_controller.rb
@@ -1,7 +1,9 @@
 class ManagerQuizzesController < ApplicationController
   def index
+    @manager = Manager.find(params[:manager_id])
+    @quizzes = Quiz.where(manager_id: @manager.id)
   end
 
   def show
-  end 
+  end
 end
